f05d5f5bccb5ee5e0eb4b1918b1d4dce46e17627
Arturo <Arturo@b1d50d5f-df8f-9249-af92-6a0d583c2fd7>
2023-06-06 08:28:06 +0000

    Se incorpora en la configuracion y el instalador una nueva propiedad para el Modo SACTA
    git-svn-id: http://10.34.2.66/svn/svn_repository/Ulises5000I/branches/agl-dev/ulises-cluster@12051 b1d50d5f-df8f-9249-af92-6a0d583c2fd7

--- ClusterSrv/Properties/Settings.Designer.cs
+++ ClusterSrv/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace ClusterSrv.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -173,6 +173,15 @@ namespace ClusterSrv.Properties {
         public int RemoteTimeout {
             get {
                 return ((int)(this["RemoteTimeout"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string SactaProxyPresent {
+            get {
+                return ((string)(this["SactaProxyPresent"]));
             }
         }
     }
